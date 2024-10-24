cd3fa00095a4463c7404b0e0714b6ed637e6094e
Anindita Widya Santoso <130077884+aninditaws@users.noreply.github.com>
2024-10-24 19:29:53 +0700

    fix: progress bar

--- frontend/styles/progress-bar.js
+++ frontend/styles/progress-bar.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const pages = {
-        "module1.html": 1,
-        "module1-quiz.html": 2,
-        "module2.html": 3,
-        "module2-quiz.html": 4,
-        "module3.html": 5,
-        "module3-quiz.html": 6,
-        "module4.html": 7,
-        "module4-quiz.html": 8
-    };
+document.addEventListener("DOMContentLoaded", function () {
+  const pages = {
+    "module1.html": 1,
+    "module1-quiz.html": 2,
+    "module2.html": 3,
+    "module2-quiz.html": 4,
+    "module3.html": 5,
+    "module3-quiz.html": 6,
+    "module4.html": 7,
+    "module4-quiz.html": 8,
+  };
 
-    const currentPage = window.location.pathname.split("/").pop();
-    const totalPages = 8;
-    const currentPageNumber = pages[currentPage] || 1;
-    const progressPercentage = ((currentPageNumber - 1) / totalPages) * 100;
+  const currentPage = window.location.pathname.split("/").pop();
+  const totalPages = 8;
+  const currentPageNumber = pages[currentPage] || 1;
+  const progressPercentage = ((currentPageNumber - 1) / totalPages) * 100;
 
-    const progressBar = document.querySelector('.progress-bar-fill');
-    progressBar.style.width = progressPercentage + '%';
+  const progressBar = document.querySelector(".progress-bar-fill");
+  progressBar.style.width = progressPercentage + "%";
 });
