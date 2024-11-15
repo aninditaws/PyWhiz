1ae1e873209d31af303be9c2c57002e2aee0ac03
aninditaws <18222128@std.stei.itb.ac.id>
2024-11-15 15:08:06 +0700

    update: api

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,14 @@
 import os
+from app import create_app
 from supabase import create_client, Client
 
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(supabase_url, supabase_key)
+
+app = create_app()
+
+app.supabase = supabase
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
